fbcdbb6081cf94f09e27fc8d0667a759829973f7
Ryan Wooster <rwooster@umich.edu>
2013-03-05 22:15:20 -0500

    Chatroom page can connect to database with AJAX.

--- common.php
+++ common.php
@@ -11,6 +11,8 @@
 		</script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js">
 		</script>
+		<script type="text/javascript" src="scripts/chat.js">
+		</script>
 		</head>
 
 		<body>
